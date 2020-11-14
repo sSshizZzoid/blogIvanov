c0c867b2d0fa71e09c13ee027fa5a21b48513e78
helloboyme <helloboyme@mail.ru>
2020-11-14 22:29:13 +0400

    Готовая 4 и 5 лаба

---
+++ blogIvanov/Controllers/BlogController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace blogIvanov.Controllers
+{
+    /// <summary>
+    /// Контроллер для работы с блогом
+    /// </summary>
+    public class BlogController : Controller
+    {
+        /// <summary>
+        /// Переход на раздел блога
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
